637429e2c381a915441be211d7dd24d7326b8dd4
abhi-verma <abhi.verma@tamu.edu>
2020-10-14 23:36:01 -0500

    fix: Change schedule for DAG to hourly

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -20,7 +20,7 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='@daily',
+          schedule_interval='@hourly',
           max_active_runs=1
         )
 
